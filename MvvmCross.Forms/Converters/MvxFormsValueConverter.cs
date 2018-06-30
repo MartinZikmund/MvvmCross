f168c0b2b98f63a0677494b46a9b45be01b565b6
Martin Zikmund <martinzikmund@sphereline.com>
2018-06-30 19:01:56 +0200

    Mvx native value converter and forms converter for MvvmCross.Forms

---
+++ MvvmCross.Forms/Converters/MvxFormsValueConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MvvmCross.Converters;
+using Xamarin.Forms;
+
+namespace MvvmCross.Forms.Converters
+{
+    public class MvxFormsValueConverter : MvxValueConverter, IValueConverter
+    {
+    }
+
+    public class MvxFormsValueConverter<TFrom> : MvxValueConverter<TFrom>, IValueConverter
+    {
+    }
+
+    public class MvxFormsValueConverter<TFrom,TTo> : MvxValueConverter<TFrom,TTo>, IValueConverter
+    {
+    }
+}
